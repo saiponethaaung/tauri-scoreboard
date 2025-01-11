cd103ad7d09cd0df00b8e9dcff1d11d64b134e03
Sai Pone Tha Aung <sai.ponethaaung@telostouch.com>
2025-01-11 17:02:46 -0500

    Update fontsize

--- src/modules/scoreboard/components/ScoreInfo.tsx
+++ src/modules/scoreboard/components/ScoreInfo.tsx
@@ -19,12 +19,12 @@ export function ScoreInfo({ team }: IProps) {
     <div className={styles.scoreInfo}>
       <div className={styles.scoreTeam}>
         <div className={styles.scoreTeamName}>{team[0].name}</div>
-        <DigitDisplay value={team[0].score} singleDigit={team[0].score < 10} />
+        <DigitDisplay value={team[0].score} singleDigit={team[0].score < 10} fontSize="24vh"/>
       </div>
       <div className={styles.scoreCompare}>X</div>
       <div className={styles.scoreTeam}>
         <div className={styles.scoreTeamName}>{team[1].name}</div>
-        <DigitDisplay value={team[1].score} singleDigit={team[1].score < 10} />
+        <DigitDisplay value={team[1].score} singleDigit={team[1].score < 10} fontSize="24vh"/>
       </div>
     </div>
   );
